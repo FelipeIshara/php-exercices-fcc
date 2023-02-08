dfe31907abca9f983b0d433720b4cddbcc8296bf
Felipe Ishara <felipeishara5@gmail.com>
2023-02-08 13:02:54 -0300

    first history

---
+++ variables/history.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        echo "The once was a man named John";
+        echo "He was 35";
+        echo "He really liked the name John";
+        echo "But didn´t like being 35";
+    ?>
+</body>
+</html>
